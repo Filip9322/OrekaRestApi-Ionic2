cc91dd396ba38e0621bac1f6c4a3b12806c03e50
filip9322 <filip9322@gmail.com>
2017-03-23 03:55:33 -0500

    Inicio del proyecto, ejemplo de internet

---
+++ src/pages/home/home.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+//Importamos el servicio de People Service
+import { PeopleService } from '../../providers/people-service';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html',
+  providers: [PeopleService]
+})
+export class HomePage {
+  //users como un array vacio
+  users: any[] = [];
+
+  constructor(
+      public navCtrl: NavController,
+      public peopleservice: PeopleService
+    ) {
+      //se llama el metodo load del service y se guarda dentro de users
+      this.peopleservice.load()
+      .then(data => {
+        this.users = data.results;
+          console.log(data.results);
+      });
+  }
+
+}
